03e6497ecdc54363cc325fb7954a68e6c7dda2b9
ann <baodeptrai712@gmail.com>
2026-04-15 11:09:15 -0600

    add semicolon

--- scripts/booking.js
+++ scripts/booking.js
@@ -25,51 +25,50 @@ let halfDay = document.getElementById("half");
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
 let dayButtons = [monday, tuesday, wednesday, thursday, friday];
-dayButtons.forEach(function (day) {
-    day.addEventListener("click", function () {
-        day.classList.toggle("clicked");
-        if (day.classList.contains("clicked")) {
-            numberOfDays++;
+dayButtons.forEach(function(day) {
+	day.addEventListener("click", function() {
+		day.classList.toggle("clicked");
+		if (day.classList.contains("clicked")) {
+			numberOfDays++;
 
-        } else {
-            numberOfDays--;
-        }
-        calculatedCost();
-    })
-})
+		} else {
+			numberOfDays--;
+		}
+		calculatedCost();
+	});
+});
 
 /********* clear days *********/
 // when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
 let costLabel = document.getElementById("calculated-cost");
-clearButton.addEventListener("click", function () {
-    dayButtons.forEach(function (day) {
-        day.classList.remove("clicked");
-    })
-    numberOfDays = 0;
-    costLabel.innerHTML = 0;
-})
+clearButton.addEventListener("click", function() {
+	dayButtons.forEach(function(day) {
+		day.classList.remove("clicked");
+	});
+	numberOfDays = 0;
+	costLabel.innerHTML = 0;
+});
 
 /********* change rate *********/
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
-halfDay.addEventListener("click", function () {
-    costPerDay = 20;
-    halfDay.classList.toggle("clicked");
-    fullDay.classList.remove("clicked");
-    calculatedCost();
-})
+halfDay.addEventListener("click", function() {
+	costPerDay = 20;
+	halfDay.classList.toggle("clicked");
+	fullDay.classList.remove("clicked");
+	calculatedCost();
+});
 
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
-fullDay.addEventListener("click", function () {
-    costPerDay = 35;
-    fullDay.classList.toggle("clicked");
-    halfDay.classList.remove("clicked");
-    calculatedCost();
-})
+fullDay.addEventListener("click", function() {
+	costPerDay = 35;
+	fullDay.classList.toggle("clicked");
+	halfDay.classList.remove("clicked");
+	calculatedCost();
+});
 
 /********* calculate *********/
 // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
 function calculatedCost() {
-    let CalculatedCost = numberOfDays * costPerDay
-    costLabel.innerHTML = CalculatedCost;
+	let CalculatedCost = numberOfDays * costPerDay;
+	costLabel.innerHTML = CalculatedCost;
 }
-
